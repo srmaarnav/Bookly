f4db2c77df20585526a946c05ad80c123441dfd2
arnav <arnav6266@gmail.com>
2025-02-27 17:00:45 +0545

    book crud functionality completed

---
+++ src/__init__.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print(f"Starting application server.")
+    await init_db()
+    yield
+    print(f"Stopping application server.")
+
+version = "v1"
+
+app = FastAPI(
+    title="Bookly",
+    description="A REST API for book review web service.",
+    version=version,
+    lifespan=life_span
+)
+
+app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
